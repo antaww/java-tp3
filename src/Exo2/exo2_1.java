711d014064e2fa3d6293f836c14c228a88d0b6ff
antaww <anta.pizzetta@gmail.com>
2022-12-31 13:47:44 +0100

    feat(exo2_2): Panel set up (need better positioning of sub elements).

--- src/Exo2/exo2_1.java
+++ src/Exo2/exo2_1.java
@@ -13,15 +13,16 @@ public class exo2_1 {
         titleLabel.setText("Exercice 2.1 - JButton");
 
         JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridBagLayout());
         JButton button1 = new JButton("Bouton simple");
-        buttonsPanel.add(button1);
         JButton button2 = new JButton("Bouton fantaisie");
         button2.setIcon(new ImageIcon(new ImageIcon("src/Exo2/canard.png").getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH)));
+
+        buttonsPanel.add(button1);
         buttonsPanel.add(button2);
         bodyPanel.add(buttonsPanel);
-        bodyPanel.setLayout(new GridLayout(3, 1));
-        buttonsPanel.setLayout(new GridLayout(2, 1));
+
+        bodyPanel.setLayout(new GridLayout(3, 1, 0, 10));
+        buttonsPanel.setLayout(new GridLayout(2, 1, 0, 5));
 
         button1.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Vous avez cliqué sur : " + button1.getText()));
         button2.addActionListener(e -> {
--- src/Exo2/exo2_2.java
+++ src/Exo2/exo2_2.java
@@ -1,6 +1,7 @@
 package Exo2;
 
 import javax.swing.*;
+import java.awt.*;
 
 import static Utils.utils.cleanBodyPanel;
 
@@ -11,6 +12,52 @@ public class exo2_2 {
         JLabel titleLabel = (JLabel) ((JPanel) ((JPanel) ((JPanel) frame.getContentPane().getComponent(0)).getComponent(1)).getComponent(0)).getComponent(0);
         titleLabel.setText("Exercice 2.2 - BoxLayout");
 
+        JPanel secondPanel = new JPanel(new BorderLayout());
+
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        JButton okButton = new JButton("OK");
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        JPanel rightPanel = new JPanel(new BorderLayout());
+        JButton cancelButton = new JButton("Cancel");
+
+
+        JPanel centerSubPanel1 = new JPanel();
+        JLabel nameLabel = new JLabel("Nom :");
+        JTextField nameTextField = new JTextField();
+        JPanel centerSubPanel2 = new JPanel();
+        JLabel passwordLabel = new JLabel("Mot de passe :");
+        JPasswordField passwordField = new JPasswordField();
+
+
+        bodyPanel.add(secondPanel);
+        secondPanel.add(leftPanel, BorderLayout.WEST);
+        secondPanel.add(centerPanel, BorderLayout.CENTER);
+        secondPanel.add(rightPanel, BorderLayout.EAST);
+
+        leftPanel.add(okButton, BorderLayout.SOUTH);
+        rightPanel.add(cancelButton, BorderLayout.SOUTH);
+        centerPanel.add(centerSubPanel1);
+        centerPanel.add(centerSubPanel2);
+        centerSubPanel1.add(nameLabel);
+        centerSubPanel1.add(nameTextField);
+        centerSubPanel2.add(passwordLabel);
+        centerSubPanel2.add(passwordField);
+
+        leftPanel.setPreferredSize(new Dimension((int) (406/1.6/2.5), 180));
+        rightPanel.setPreferredSize(new Dimension((int) (406/1.6/2.5), 180));
+
+        secondPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        leftPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+        centerPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+        rightPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        centerSubPanel1.setBorder(BorderFactory.createLineBorder(Color.RED));
+        centerSubPanel2.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+
+        bodyPanel.setLayout(new GridLayout(2, 1));
+
         frame.setVisible(true);
+        System.out.println(secondPanel.getWidth());
+        System.out.println(secondPanel.getHeight());
     }
 }
