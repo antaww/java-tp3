2c4fdd87bd6333dfbec680690a36602d5d9890d3
antaww <anta.pizzetta@gmail.com>
2023-01-02 18:04:44 +0100

    feat(exo4_1): Exo4.1 done (need todo).

---
+++ src/main/java/fr/ynov/tp3/PExo4/SpecialCards.java
@@ -0,0 +1,51 @@
+package fr.ynov.tp3.PExo4;
+
+import fr.ynov.tp3.Card;
+
+public class SpecialCards implements Card {
+    private String name;
+    private String type; // Magie, Piège
+    private SpecialIcon specialIcon; // Continu, Contre, Équipement, Jeu_Rapide, Rituel, Terrain
+    private String reference; // XXXX-XXXXX
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return "[" + type + "]";
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public SpecialIcon getSpecialIcon() {
+        return specialIcon;
+    }
+
+    public void setSpecialIcon(SpecialIcon specialIcon) {
+        this.specialIcon = specialIcon;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/fr/ynov/tp3/PExo4/SpecialIcon.java
@@ -0,0 +1,10 @@
+package fr.ynov.tp3.PExo4;
+
+public enum SpecialIcon {
+    Continu,
+    Contre,
+    Equipement,
+    Jeu_Rapide,
+    Rituel,
+    Terrain
+}
