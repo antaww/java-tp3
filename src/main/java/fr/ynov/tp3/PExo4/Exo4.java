cc1b74417e663f903bca61ad466379e63b4f5789
antaww <anta.pizzetta@gmail.com>
2023-01-13 17:42:38 +0100

    feat(exo4): Add combobox to exo4 + adapted menu for spells & traps.

--- src/main/java/fr/ynov/tp3/PExo4/Exo4.java
+++ src/main/java/fr/ynov/tp3/PExo4/Exo4.java
@@ -1,63 +1,69 @@
 package fr.ynov.tp3.PExo4;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
 import javax.swing.*;
 import java.awt.*;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.ArrayList;
 
 import static fr.ynov.tp3.PUtils.Utils.*;
 
 public class Exo4 {
-    public static void main(JFrame frame, String specialType) {
+    public static void main(JFrame frame, String title) {
         var bodyPanel = (JPanel) ((JPanel) frame.getContentPane().getComponent(0)).getComponent(1);
         cleanBodyPanel(bodyPanel);
         var titleLabel = (JLabel) ((JPanel) ((JPanel) ((JPanel) frame.getContentPane().getComponent(0)).getComponent(1)).getComponent(0)).getComponent(0);
-        switch (specialType) {
-            case "Carte Magie" -> titleLabel.setText("Exercice 4.1 - Yu-Gi-Oh!");
-            case "Carte Piège" -> titleLabel.setText("Exercice 4.2 - Yu-Gi-Oh!");
+
+        var titles = new ArrayList<String>();
+        titles.add(title);
+        switch (title) {
+            case "Carte Magie" -> {
+                titleLabel.setText("Exercice 4.1 - Yu-Gi-Oh!");
+                System.out.println(titleLabel.getText());
+                titles.add("Spell");
+            }
+            case "Carte Piège" -> {
+                titleLabel.setText("Exercice 4.2 - Yu-Gi-Oh!");
+                System.out.println(titleLabel.getText());
+                titles.add("Trap");
+            }
         }
 
-        var cardPanel = createCardPanel(bodyPanel, specialType);
-        var displayButton = (JButton) cardPanel.get("displayButton");
+        var cardPanel = createCardPanel(bodyPanel, titles.get(0));
+        var comboBox = (JComboBox<String>) cardPanel.get("comboBox");
         var resultLabel = (JLabel) cardPanel.get("resultLabel");
         var resultImagePanel = (JPanel) cardPanel.get("resultImagePanel");
         var resultPanel = (JPanel) cardPanel.get("resultPanel");
 
         resultLabel.setFont(new Font("Arial", Font.BOLD, 15));
 
-        final Boolean[] isDisplayButtonClicked = {false};
+        var jsonElement = getJsonElement(comboBox, titles.get(1));
 
-        displayButton.addActionListener(e -> {
-            if (!isDisplayButtonClicked[0]) {
-                isDisplayButtonClicked[0] = true;
-                JsonElement jsonElement;
-                try {
-                    jsonElement = JsonParser.parseReader(new FileReader("src/main/resources/cards.json"));
-                } catch (FileNotFoundException ex) {
-                    throw new RuntimeException(ex);
+        comboBox.addActionListener(e -> {
+            resultImagePanel.removeAll();
+            var selected = comboBox.getSelectedItem();
+            var spellCard1 = new SpecialCards();
+
+            jsonElement.getAsJsonArray().forEach(jsonElement1 -> {
+                var cardName = jsonElement1.getAsJsonObject().get("name").getAsString();
+                if (cardName.equals(selected)) {
+                    var cardType = replaceByUnderscore(jsonElement1.getAsJsonObject().get("type").getAsString());
+                    var cardRace = replaceByUnderscore(jsonElement1.getAsJsonObject().get("race").getAsString());
+                    var cardReference = jsonElement1.getAsJsonObject().get("card_sets").getAsJsonArray().get(0).getAsJsonObject().get("set_code").getAsString();
+                    var cardDescription = jsonElement1.getAsJsonObject().get("desc").getAsString();
+                    var cardImage = jsonElement1.getAsJsonObject().get("card_images").getAsJsonArray().get(0).getAsJsonObject().get("image_url").getAsString();
+                    setSpecialCard(spellCard1, cardName, SpecialType.valueOf(cardType), SpecialIcon.valueOf(cardRace), cardReference, cardDescription);
+                    displayCardImage(resultLabel, resultImagePanel, resultPanel, cardImage);
                 }
-                switch (specialType) {
-                    case "Carte Magie" -> {
-                        var spellCard1 = new SpecialCards();
-                        createAndDisplaySpecialCard(spellCard1, "Typhon d'Espace Mystique", displayButton, resultLabel, resultImagePanel, resultPanel, jsonElement);
-                    }
-                    case "Carte Piège" -> {
-                        var trapCard1 = new SpecialCards();
-                        createAndDisplaySpecialCard(trapCard1, "Sortilège de l'ombre", displayButton, resultLabel, resultImagePanel, resultPanel, jsonElement);
-                    }
-                }
-            } else {
-                isDisplayButtonClicked[0] = false;
-                displayButton.setText("Afficher la carte");
-                resultLabel.setText("Cliquer sur le bouton pour afficher la carte...");
-                resultPanel.setLayout(new FlowLayout());
-                resultLabel.setHorizontalAlignment(SwingConstants.CENTER);
-                resultImagePanel.removeAll();
-                resultImagePanel.setVisible(false);
-            }
+            });
+            resultLabel.setText(hasUnderscore("<html>" +
+                    "<div>" +
+                    "<p><u>Nom</u> : " + spellCard1.getName() +
+                    "<br><u>Type</u> : " + spellCard1.getType() +
+                    "<br><u>Icône</u> : " + spellCard1.getSpecialIcon() +
+                    "<br><u>Référence</u> : " + spellCard1.getReference().toUpperCase() +
+                    "<br><u>Description</u> : " + spellCard1.getDescription() +
+                    "</div>" +
+                    "</html>")
+            );
         });
         frame.setVisible(true);
     }
