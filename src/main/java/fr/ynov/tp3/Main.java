79f04328abeb1d4a8ebfab22aa8d95902a77d53e
antaww <anta.pizzetta@gmail.com>
2023-01-24 20:20:31 +0100

    docs(main): Add documentation to main.

--- src/main/java/fr/ynov/tp3/Main.java
+++ src/main/java/fr/ynov/tp3/Main.java
@@ -2,7 +2,15 @@ package fr.ynov.tp3;
 
 import fr.ynov.tp3.PUtils.Menu;
 
+/**
+ * Classe Main : création de la classe principale du programme qui lance le programme et appelle la méthode main de la classe Menu.
+ */
 public class Main {
+    /**
+     * Méthode main : méthode principale du programme qui lance le programme et appelle la méthode main de la classe Menu.
+     *
+     * @param args arguments de la méthode main
+     */
     public static void main(final String[] args) {
         Menu.main(args);
     }
